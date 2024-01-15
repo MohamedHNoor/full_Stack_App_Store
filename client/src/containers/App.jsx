69fa93d10e373ac59d65f0218cf1c700dd9e72f3
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-01-15 11:31:36 +0200

    Getting JWT token

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,10 +1,25 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout, AdminLayout, AuthLayout } from '../layouts';
+import { auth } from '../config/firebase.config';
 
 import { Home, Profile, AdminHome, Authentication } from '../pages';
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (userCred) => {
+      if (userCred) {
+        try {
+          const token = await userCred.getIdToken();
+          console.log(token);
+        } catch (error) {
+          console.error('Error fetching ID token:', error);
+        }
+      }
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
   return (
     <Suspense fallback={<div>loading...</div>}>
       <Routes>
--- client/src/pages/Authentication.jsx
+++ client/src/pages/Authentication.jsx
@@ -1,7 +1,22 @@
+import React, { useCallback } from 'react';
 import { LoginBG } from '../assets';
 import { FcGoogle } from 'react-icons/fc';
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+import { auth } from '../config/firebase.config';
 
 const Authentication = () => {
+  const googleProvider = new GoogleAuthProvider();
+  const handleLoginAction = useCallback(async () => {
+    try {
+      const userCred = await signInWithRedirect(auth, googleProvider);
+      if (userCred) {
+        console.log(userCred);
+      }
+    } catch (error) {
+      console.error('Error during login', error);
+    }
+  }, []);
+
   return (
     <div
       style={{
@@ -21,7 +36,10 @@ const Authentication = () => {
           <p className='text-lg text-gray-400'>Sign in to access your store</p>
         </div>
 
-        <div className='w-full lg:w-auto px-4 py-3 rounded-md flex items-center justify-center border border-gray-200 cursor-pointer gap-4 active:scale-95 transition-all duration-150 ease-in-out'>
+        <div
+          onClick={handleLoginAction}
+          className='w-full lg:w-auto px-4 py-3 rounded-md flex items-center justify-center border border-gray-200 cursor-pointer gap-4 active:scale-95 transition-all duration-150 ease-in-out'
+        >
           <FcGoogle className='text-3xl' />
           <p className='text-lg font-semibold text-white'>Sign in with Gmail</p>
         </div>
