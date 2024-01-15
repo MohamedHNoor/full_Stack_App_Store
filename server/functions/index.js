b5df8dcde1ace36ad67273b7b6fde817440873de
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-01-15 12:47:07 +0200

    Add Firebase cloud functions and validated user jwt token

---
+++ server/functions/index.js
@@ -0,0 +1,40 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+const cors = require('cors')({ origin: true });
+
+// initialize the admin
+admin.initializeApp();
+
+// initialize the db instance
+const db = admin.firestore();
+
+// function to validate the user JWT Bearer Token
+
+exports.validateUserJWTToken = functions.https.onRequest(async (req, res) => {
+  // enable cors
+  cors(req, res, async () => {
+    // get Authorization header from the request
+    const authorizationHeader = req.get('Authorization');
+
+    // check if the Authorization header is present
+    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer')) {
+      return res.status(401).json({ error: 'Unauthorize' });
+    }
+
+    // extract the token from the Authorization header
+    const token = authorizationHeader.split('Bearer ')[1];
+
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(token);
+      if (decodedToken) {
+        return res.status(200).json({ success: true, user: decodedToken });
+      }
+    } catch (error) {
+      console.log('Error on validating', error);
+      return res
+        .status(401)
+        .json({ error: error.message, status: 'Unauthorized' });
+    }
+  });
+});
